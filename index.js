9bb3dc3e75a8b67f9ce01397ba7187a4dd280805
JulianeWe <yuli@wl-01145.WLAN.LOCAL>
2017-01-27 12:01:57 +0100

    added files

---
+++ index.js
@@ -0,0 +1,84 @@
+
+"use strict"; // Must always be the first line.
+
+//Required for Sphero.
+var sphero = require("sphero");
+var orb = sphero("/dev/tty.Sphero-ORG-AMP-SPP");
+//Required for server.
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+var conf = require("./config.json");
+
+console.log("Starting spheroproject");
+
+server.listen(conf.port);
+//File of public, handed over to visitor.
+app.configure(function(){
+   app.use(express.static(__dirname + "/public")); 
+});
+app.get("/", function (req, res) {
+  res.sendfile(__dirname + "/public/index.html");
+});
+
+//Belongs to the websocket.
+io.sockets.on("connection", function (socket) {
+  //Client connected.
+  //socket.emit("update", { batteryState: 100 , batteryVoltage: 45});
+});
+
+
+
+
+
+
+
+//Set color to blue when connected to BT.
+orb.connect(function() {
+   orb.color("#0404B4");
+
+   setTimeout(function(){
+        orb.getPowerState(function(err, data) {
+          if (err) {
+            console.log("error: ", err);
+          } else {
+            io.sockets.emit("updateBattery", data);
+            console.log("data:");
+            console.log("  recVer:", data.recVer);
+            console.log("  batteryState:", data.batteryState);
+            console.log("  batteryVoltage:", data.batteryVoltage);
+            console.log("  chargeCount:", data.chargeCount);
+            console.log("  secondsSinceCharge:", data.secondsSinceCharge);
+          }
+
+
+          //Calibration of Sphero - All other things that Sphero should do after
+          //Calibration need to be entered here!
+          orb.startCalibration();
+          setTimeout(function() {
+            orb.finishCalibration();
+
+            //Gyroscope data of Sphero
+            orb.streamGyroscope(1,false);
+            orb.on("gyroscope", function(data) {
+              io.sockets.emit("updateGyro", data);
+            });
+
+            //Streaming of IMU (inertial measurment unit) data
+            orb.streamImuAngles(1, false);
+            orb.on("imuAngles", function(data) {
+              io.sockets.emit("updateImu", data);
+            });
+
+
+          }, 10000); //10 Sek
+      });
+
+    },10000); //10s
+
+  });//orb.connect
+ 
+
+
+
---
+++ public/client.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// Client.js gets the data from Websocket-Server which has got the data of the Sphero
+// Client.js will show those data in a browser.
+
+$(document).ready(function(){
+    var socket = io.connect();
+
+var svg = d3.select("svg");
+svg.append("rect").attr("width", 10).attr("height",50).attr("x",50).attr("y",250);
+svg.append("rect").attr("width", 10).attr("height",50).attr("x",150).attr("y",250);
+svg.append("rect").attr("width", 10).attr("height",50).attr("x",250).attr("y",250);
+
+
+
+    socket.on("updateBattery", function(data){
+    //console.log(data);
+    $("#Battery").text(data.batteryState);
+    });
+
+    socket.on("updateGyro", function(data) {
+        //console.log(data);
+        $("#xGyro").text("x:" + data.xGyro.value[0]);
+        $("#yGyro").text("y:" + data.yGyro.value[0]);
+        $("#zGyro").text("z:" + data.zGyro.value[0]);
+    });
+
+    socket.on("updateImu", function(data) {
+        //console.log(data);
+        $("#pitch").text("Pitch:" + data.pitchAngle.value[0] + "°");
+        $("#roll").text("Roll:" + data.rollAngle.value[0] + "°");
+        $("#yaw").text("Yaw:" + data.yawAngle.value[0] + "°");
+        var dataArray=[data.pitchAngle.value[0],data.rollAngle.value[0],data.yawAngle.value[0]];
+
+        //Transform the data from dataArray into useful stuff for svg objects.
+        svg.selectAll("rect")
+            .data(dataArray)
+            .enter();
+        svg.selectAll("rect").attr("y",function(d){return d + 180;});
+
+        
+    });
+
+
+
+
+
+});
+
